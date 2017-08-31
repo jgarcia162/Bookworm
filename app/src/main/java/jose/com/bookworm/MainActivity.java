270a05b02ff01aa5e5c0bd87594df0b898b441af
Jose Garcia <jogarcia809@yahoo.com>
2017-08-31 08:47:56 -0400

    initial commit

---
+++ app/src/main/java/jose/com/bookworm/MainActivity.java
@@ -0,0 +1,14 @@
+package jose.com.bookworm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("BookWorm");
+    }
+}
