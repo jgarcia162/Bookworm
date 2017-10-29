866b29a72469463c71002fcb2a1aac862af81e01
Jose Garcia <jogarcia809@yahoo.com>
2017-10-29 16:09:55 -0400

    created Library activity for testing purposes

--- app/src/main/java/jose/com/bookworm/Library.java
+++ app/src/main/java/jose/com/bookworm/Library.java
@@ -1,8 +1,24 @@
 package jose.com.bookworm;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import jose.com.bookworm.model.Book;
+
 /**
  * Created by Joe on 10/29/17.
  */
 
-class Library {
+public class Library extends AppCompatActivity{
+    Box<Book> bookBox;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BoxStore boxStore = ((App) getApplication()).getBoxStore();
+        bookBox = boxStore.boxFor(Book.class);
+
+    }
 }
